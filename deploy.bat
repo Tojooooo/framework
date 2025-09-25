c0e3e2d2185fda1258b7e230625be497ddaf81fe
Tojooooo <155669487+Tojooooo@users.noreply.github.com>
2025-09-25 05:28:06 +0300

    initial commit

---
+++ deploy.bat
@@ -0,0 +1,45 @@
+@echo off
+
+:: Variables pour la librairie FrontServlet
+set APP_NAME=framework
+set SRC_DIR=src\main\java
+set BUILD_DIR=build
+set LIB_DIR=lib
+set SERVLET_API_JAR=%LIB_DIR%\servlet-api.jar
+set TEST_LIB_DIR=..\framework-test\lib
+
+:: Nettoyage
+if exist %BUILD_DIR% (
+    rmdir /s /q %BUILD_DIR%
+)
+mkdir %BUILD_DIR%
+
+:: Compilation
+echo Compilation de la librairie java...
+dir /b /s %SRC_DIR%\*.java > sources.txt
+javac -cp "%SERVLET_API_JAR%" -d %BUILD_DIR% @sources.txt
+if errorlevel 1 (
+    echo Erreur de compilation!
+    del sources.txt
+    pause
+    exit /b 1
+)
+del sources.txt
+
+:: Création du JAR
+echo Creation du JAR %APP_NAME%.jar...
+cd %BUILD_DIR%
+jar -cvf %APP_NAME%.jar com
+cd ..
+
+:: Copie vers le projet Test
+if not exist %TEST_LIB_DIR% (
+    mkdir %TEST_LIB_DIR%
+)
+copy /Y %BUILD_DIR%\%APP_NAME%.jar %TEST_LIB_DIR%\
+
+echo.
+echo Librairie compilation succes!
+echo Copie vers: %TEST_LIB_DIR%\%APP_NAME%.jar
+echo.
+pause
