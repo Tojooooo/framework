ee3a2cdf2cfe4045afc76dc22575861ada89ba9a
Tojooooo <tojonirina.rakotobe@yahoo.com>
2025-12-13 15:04:59 +0300

    argument entite dans methode d action

--- src/main/java/mg/tojooooo/framework/RouterEngine.java
+++ src/main/java/mg/tojooooo/framework/RouterEngine.java
@@ -1,12 +1,16 @@
 package mg.tojooooo.framework;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Parameter;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -77,34 +81,16 @@ public class RouterEngine {
         Parameter[] params = mth.getParameters();
         Object[] paramValues = new Object[params.length];
         ModelMapper modelMapper = new ModelMapper();
+        
         for (int i = 0; i < params.length; i++) {
             Class<?> paramType = params[i].getType();
             // Object requestParamValue = request.getParameter(params[i].getName());
             if (params[i].isAnnotationPresent(PathParam.class)) {
-                PathParam pp = params[i].getAnnotation(PathParam.class);
-                String[] urlChunks = routeMapping.getUrl().split("/");
-                String[] requestUrlChunks = request.getRequestURI().substring(request.getContextPath().length()).split("/");
-                for (int j = 0; j < urlChunks.length; j++) {
-                    if (urlChunks[j].startsWith("{") && urlChunks[j].endsWith("}")) {
-                        urlChunks[j] = urlChunks[j].replace("{", "");
-                        urlChunks[j] = urlChunks[j].replace("}", "");
-                        if (urlChunks[j].startsWith(pp.value())) {
-                            if (paramType == Boolean.class) {
-                                paramValues[i] = Boolean.parseBoolean(requestUrlChunks[j]);
-                            } else {
-                                paramValues[i] = modelMapper.map(requestUrlChunks[j], paramType);
-                            }
-                            break;
-                        }
-                    }
-                }
+                Object value = handlePathParam(request, routeMapping, params[i], paramType, modelMapper);
+                paramValues[i] = convertValue(value.toString(), paramType);
             } else if (params[i].isAnnotationPresent(RequestParam.class)) {
                 RequestParam a = params[i].getAnnotation(RequestParam.class);
-                if (paramType == Boolean.class) {
-                    paramValues[i] = Boolean.parseBoolean(request.getParameter(a.value()));
-                } else {
-                    paramValues[i] = modelMapper.map(request.getParameter(a.value()), paramType);
-                }
+                paramValues[i] = convertValue(request.getParameter(a.value()), paramType);
             } else if (params[i].getType() == Map.class) {
                 ParameterizedType genericType = (ParameterizedType) params[i].getParameterizedType();
                 Type[] arrTypes = genericType.getActualTypeArguments();
@@ -113,12 +99,10 @@ public class RouterEngine {
                 } else {
                     throw new Exception("Type de map inattendu. doit etre Map<String, Object>");
                 }
+            } else if (isCustomObject(paramType)) {
+                paramValues[i] = createCustomObject(request, paramType, "");
             } else {
-                if (paramType == Boolean.class) {
-                    paramValues[i] = Boolean.parseBoolean(request.getParameter(params[i].getName()));
-                } else {
-                    paramValues[i] = modelMapper.map(request.getParameter(params[i].getName()), paramType);
-                }
+                paramValues[i] = modelMapper.map(request.getParameter(params[i].getName()), paramType);
             }
         }
         return paramValues;
@@ -130,6 +114,236 @@ public class RouterEngine {
             m.put(paramName, modelMapper.map(request.getParameter(paramName), Object.class));
         }
         return m;
+    }
+
+    private Object handlePathParam(HttpServletRequest request, RouteMapping routeMapping, 
+                                Parameter param, Class<?> paramType, ModelMapper modelMapper) {
+        PathParam pp = param.getAnnotation(PathParam.class);
+        String[] urlChunks = routeMapping.getUrl().split("/");
+        String[] requestUrlChunks = request.getRequestURI().substring(request.getContextPath().length()).split("/");
+        for (int i = 0; i < urlChunks.length; i++) {
+            if (urlChunks[i].startsWith("{") && urlChunks[i].endsWith("}")) {
+                urlChunks[i] = urlChunks[i].replace("{", "");
+                urlChunks[i] = urlChunks[i].replace("}", "");
+                if (urlChunks[i].startsWith(pp.value())) {
+                    return convertValue(requestUrlChunks[i], paramType);
+                }
+            }
+        }
+        return null;
+    }
+
+    private Map<String, Object> handleMapParam(HttpServletRequest request, Parameter param, 
+                                            ModelMapper modelMapper) {
+        Map<String, Object> map = new HashMap<>();
+        Enumeration<String> paramNames = request.getParameterNames();
+        
+        while (paramNames.hasMoreElements()) {
+            String paramName = paramNames.nextElement();
+            String[] values = request.getParameterValues(paramName);
+            
+            if (values != null) {
+                map.put(paramName, values.length == 1 ? values[0] : values);
+            }
+        }
+        
+        return map;
+    }
+
+    // Vérifie si c'est un objet personnalisé
+    private boolean isCustomObject(Class<?> clazz) {
+        // System.out.println("custom: "+ clazz.getName());
+        // Simplifié : tout ce qui n'est pas type Java standard est considéré comme custom
+        String packageName = clazz.getPackage() != null ? clazz.getPackage().getName() : "";
+
+        return !clazz.isPrimitive() 
+            && !clazz.isArray() 
+            && !Collection.class.isAssignableFrom(clazz)
+            && !Map.class.isAssignableFrom(clazz)
+            && !packageName.startsWith("java.")
+            && !packageName.startsWith("javax.");
+    }
+
+    private Object createCustomObject(HttpServletRequest request, Class<?> clazz, String prefix) {
+        try {
+            Object instance = null;
+            try {
+                // classe interne statique
+                instance = clazz.getDeclaredConstructor().newInstance();
+            } catch (NoSuchMethodException e1) {
+                System.err.println("Impossible de créer une instance de " + clazz.getName());
+                return null;
+            }
+            
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                Class<?> fieldType = field.getType();
+                String fieldName = field.getName();
+                
+                // nom du paramètre
+                String paramName = prefix.isEmpty() ? fieldName : prefix + "." + fieldName;
+                
+                // Vérifie si c'est une liste
+                if (List.class.isAssignableFrom(fieldType)) {
+                    // type générique de la liste
+                    Type genericType = field.getGenericType();
+                    if (genericType instanceof ParameterizedType) {
+                        ParameterizedType pt = (ParameterizedType) genericType;
+                        Type[] typeArgs = pt.getActualTypeArguments();
+                        if (typeArgs.length > 0 && typeArgs[0] instanceof Class) {
+                            Class<?> listItemType = (Class<?>) typeArgs[0];
+                            
+                            List<Object> list = createObjectList(request, listItemType, paramName);
+                            field.set(instance, list);
+                        }
+                    }
+                }
+                // recursion si field objet personnalisé
+                else if (isCustomObject(fieldType)) {
+                    // Appel récursif
+                    Object nestedObject = createCustomObject(request, fieldType, paramName);
+                    field.set(instance, nestedObject);
+                }
+                // Vérifie si un tableau pour checkboxes)
+                else if (fieldType.isArray()) {
+                    Object array = createArrayFromRequest(request, fieldType, paramName);
+                    if (array != null) {
+                        field.set(instance, array);
+                    }
+                }
+                // Type simple (String, Integer, etc.)
+                else {
+                    String paramValue = request.getParameter(paramName);
+                    if (paramValue != null) {
+                        field.set(instance, convertValue(paramValue, fieldType));
+                    }
+                    
+                    // Gestion checkboxes booléennes
+                    if ((fieldType == Boolean.class || fieldType == boolean.class) 
+                        && request.getParameter(paramName + ".checkbox") != null) {
+                        String checkboxValue = request.getParameter(paramName);
+                        boolean isChecked = "on".equals(checkboxValue) || "true".equals(checkboxValue);
+                        field.set(instance, isChecked);
+                    }
+                }
+            }
+            
+            return instance;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Crée liste d'objets à partir des paramètres de la requête
+    private List<Object> createObjectList(HttpServletRequest request, Class<?> itemType, String prefix) {
+        List<Object> list = new ArrayList<>();
+        
+        // Cherche tous les indices dans les paramètres
+        Set<Integer> indices = new HashSet<>();
+        Enumeration<String> paramNames = request.getParameterNames();
+        
+        while (paramNames.hasMoreElements()) {
+            String paramName = paramNames.nextElement();
+            
+            // dept[0] ohatra
+            if (paramName.startsWith(prefix + "[") && paramName.contains("]")) {
+                try {
+                    // Extraire l'index
+                    int startBracket = paramName.indexOf("[");
+                    int endBracket = paramName.indexOf("]", startBracket);
+                    String indexStr = paramName.substring(startBracket + 1, endBracket);
+                    int index = Integer.parseInt(indexStr);
+                    indices.add(index);
+                } catch (Exception e) {
+                    // Ignorer les paramètres mal formés
+                }
+            }
+        }
+        
+        // crée un objet pour chaque indice
+        for (int index : indices) {
+            String itemPrefix = prefix + "[" + index + "]";
+            
+            if (isCustomObject(itemType)) {
+                // Objet personnalisé
+                Object item = createCustomObject(request, itemType, itemPrefix);
+                if (item != null) {
+                    while (list.size() <= index) {
+                        list.add(null);
+                    }
+                    list.set(index, item);
+                }
+            } else {
+                // Type simple
+                String paramName = prefix + "[" + index + "]";
+                String value = request.getParameter(paramName);
+                if (value != null) {
+                    while (list.size() <= index) {
+                        list.add(null);
+                    }
+                    list.set(index, convertValue(value, itemType));
+                }
+            }
+        }
+        
+        return list;
+    }
+
+    // Crée un tableau à partir des valeurs d'un paramètre (pour les checkboxes multiples)
+    private Object createArrayFromRequest(HttpServletRequest request, Class<?> arrayType, String paramName) {
+        String[] values = request.getParameterValues(paramName);
+        if (values == null || values.length == 0) {
+            return null;
+        }
+        
+        Class<?> componentType = arrayType.getComponentType();
+        
+        if (componentType == String.class) {
+            return values;
+        } else if (componentType == int.class) {
+            int[] array = new int[values.length];
+            for (int i = 0; i < values.length; i++) {
+                array[i] = Integer.parseInt(values[i]);
+            }
+            return array;
+        } else if (componentType == Integer.class) {
+            Integer[] array = new Integer[values.length];
+            for (int i = 0; i < values.length; i++) {
+                array[i] = Integer.parseInt(values[i]);
+            }
+            return array;
+        } else if (componentType == boolean.class) {
+            boolean[] array = new boolean[values.length];
+            for (int i = 0; i < values.length; i++) {
+                array[i] = Boolean.parseBoolean(values[i]);
+            }
+            return array;
+        } else if (componentType == Boolean.class) {
+            Boolean[] array = new Boolean[values.length];
+            for (int i = 0; i < values.length; i++) {
+                array[i] = Boolean.parseBoolean(values[i]);
+            }
+            return array;
+        }
+        
+        return null;
+    }
+
+    private Object convertValue(String value, Class<?> targetType) {
+        if (value == null) return null;
+        if (targetType == Boolean.class || targetType == boolean.class) return Boolean.parseBoolean(value);
+
+        try {
+            ModelMapper mm = new ModelMapper();
+            return mm.map(value, targetType);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private RouteMapping getRouteMapping(String url, HttpServletRequest request) {
